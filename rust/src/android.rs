f7331081b6e55abecb4dd2b6b3de625da5c218fa
Yehor Popovych <ypopovych@crossroad.io>
2018-10-20 20:17:06 +0300

    Better exception handling

--- rust/src/android.rs
+++ rust/src/android.rs
@@ -3,6 +3,7 @@ use cardano;
 
 use std::str;
 use std::ptr;
+use std::panic;
 
 // This is the interface to the JVM that we'll
 // call the majority of our methods on.
@@ -45,19 +46,31 @@ fn json_object_to_string(env: &JNIEnv, obj: JObject) -> String {
   env.get_string(jstr.into()).expect("Couldn't get java string!").into()
 }
 
+fn handle_exception<F: FnOnce(&JNIEnv) -> *mut R + panic::UnwindSafe, R>(env: JNIEnv, func: F) -> *mut R {
+  match panic::catch_unwind(|| func(&env)) {
+    Ok(res) => res,
+    Err(err) => {
+      env.throw(format!("Error: {:?}", err)).unwrap();
+      ptr::null_mut()
+    }
+  }
+}
+
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern fn Java_io_crossroad_rncardano_Native_hdWalletFromEnhancedEntropy(
   env: JNIEnv, _: JObject, bytes: jbyteArray, password: JString
 ) -> jbyteArray {
-  let input = env.convert_byte_array(bytes).unwrap();
-  let pwd_str: String = env.get_string(password).expect("Couldn't get java string!").into();
-  let pwd: &[u8] = pwd_str.as_bytes();
-  let mut output = [0 as u8; cardano::hdwallet::XPRV_SIZE];
+  handle_exception(env, |env| {
+    let input = env.convert_byte_array(bytes).unwrap();
+    let pwd_str: String = env.get_string(password).expect("Couldn't get java string!").into();
+    let pwd: &[u8] = pwd_str.as_bytes();
+    let mut output = [0 as u8; cardano::hdwallet::XPRV_SIZE];
 
-  wallet_from_enhanced_entropy(input.as_ptr(), input.len(), pwd.as_ptr(), pwd.len(), output.as_mut_ptr());
+    wallet_from_enhanced_entropy(input.as_ptr(), input.len(), pwd.as_ptr(), pwd.len(), output.as_mut_ptr());
 
-  env.byte_array_from_slice(&output).unwrap()
+    env.byte_array_from_slice(&output).unwrap()
+  })
 }
 
 #[allow(non_snake_case)]
@@ -65,12 +78,14 @@ pub extern fn Java_io_crossroad_rncardano_Native_hdWalletFromEnhancedEntropy(
 pub extern fn Java_io_crossroad_rncardano_Native_walletFromMasterKey(
   env: JNIEnv, _: JObject, bytes: jbyteArray
 ) -> jobject {
-  let input = env.convert_byte_array(bytes).unwrap();
-  let mut output = [0 as u8; MAX_OUTPUT_SIZE];
+  handle_exception(env, |env| {
+    let input = env.convert_byte_array(bytes).unwrap();
+    let mut output = [0 as u8; MAX_OUTPUT_SIZE];
 
-  let rsz = xwallet_from_master_key(input.as_ptr(), output.as_mut_ptr()) as usize;
+    let rsz = xwallet_from_master_key(input.as_ptr(), output.as_mut_ptr()) as usize;
 
-  json_string_to_object(&env, &output[0..rsz])
+    json_string_to_object(&env, &output[0..rsz])
+  })
 }
 
 #[allow(non_snake_case)]
@@ -78,13 +93,15 @@ pub extern fn Java_io_crossroad_rncardano_Native_walletFromMasterKey(
 pub extern fn Java_io_crossroad_rncardano_Native_walletNewAccount(
   env: JNIEnv, _: JObject, params: JObject
 ) -> jobject {
-  let string = json_object_to_string(&env, params);
-  let input = string.as_bytes();
-  let mut output = [0 as u8; MAX_OUTPUT_SIZE];
+  handle_exception(env, |env| {
+    let string = json_object_to_string(&env, params);
+    let input = string.as_bytes();
+    let mut output = [0 as u8; MAX_OUTPUT_SIZE];
 
-  let rsz = xwallet_account(input.as_ptr(), input.len(), output.as_mut_ptr()) as usize;
+    let rsz = xwallet_account(input.as_ptr(), input.len(), output.as_mut_ptr()) as usize;
 
-  json_string_to_object(&env, &output[0..rsz])
+    json_string_to_object(&env, &output[0..rsz])
+  })
 }
 
 #[allow(non_snake_case)]
@@ -92,13 +109,15 @@ pub extern fn Java_io_crossroad_rncardano_Native_walletNewAccount(
 pub extern fn Java_io_crossroad_rncardano_Native_randomAddressCheckerNewChecker(
   env: JNIEnv, _: JObject, pkey: JObject
 ) -> jobject {
-  let string = json_object_to_string(&env, pkey);
-  let input = string.as_bytes();
-  let mut output = [0 as u8; MAX_OUTPUT_SIZE];
+  handle_exception(env, |env| {
+    let string = json_object_to_string(&env, pkey);
+    let input = string.as_bytes();
+    let mut output = [0 as u8; MAX_OUTPUT_SIZE];
 
-  let rsz = random_address_checker_new(input.as_ptr(), input.len(), output.as_mut_ptr()) as usize;
+    let rsz = random_address_checker_new(input.as_ptr(), input.len(), output.as_mut_ptr()) as usize;
 
-  json_string_to_object(&env, &output[0..rsz])
+    json_string_to_object(&env, &output[0..rsz])
+  })
 }
 
 #[allow(non_snake_case)]
@@ -106,13 +125,15 @@ pub extern fn Java_io_crossroad_rncardano_Native_randomAddressCheckerNewChecker(
 pub extern fn Java_io_crossroad_rncardano_Native_randomAddressCheckerCheckAddresses(
   env: JNIEnv, _: JObject, params: JObject
 ) -> jobject {
-  let string = json_object_to_string(&env, params);
-  let input = string.as_bytes();
-  let mut output = [0 as u8; MAX_OUTPUT_SIZE];
+  handle_exception(env, |env| {
+    let string = json_object_to_string(&env, params);
+    let input = string.as_bytes();
+    let mut output = [0 as u8; MAX_OUTPUT_SIZE];
 
-  let rsz = random_address_check(input.as_ptr(), input.len(), output.as_mut_ptr()) as usize;
+    let rsz = random_address_check(input.as_ptr(), input.len(), output.as_mut_ptr()) as usize;
 
-  json_string_to_array(&env, &output[0..rsz])
+    json_string_to_array(&env, &output[0..rsz])
+  })
 }
 
 #[allow(non_snake_case)]
@@ -122,29 +143,31 @@ pub extern fn Java_io_crossroad_rncardano_Native_passwordProtectEncryptWithPassw
 ) -> jbyteArray {
   use self::password_encryption_parameter::*;
 
-  let nsalt = env.convert_byte_array(salt).unwrap();
-  let nnonce = env.convert_byte_array(nonce).unwrap();
+  handle_exception(env, |env| {
+    let nsalt = env.convert_byte_array(salt).unwrap();
+    let nnonce = env.convert_byte_array(nonce).unwrap();
 
-  if nsalt.len() != SALT_SIZE { panic!("Wrong salt len {} should be {}", nsalt.len(), SALT_SIZE); }
-  if nnonce.len() != NONCE_SIZE { panic!("Wrong nonce len {} should be {}", nnonce.len(), NONCE_SIZE) }
-  
-  let ndata = env.convert_byte_array(data).unwrap();
+    if nsalt.len() != SALT_SIZE { panic!("Wrong salt len {} should be {}", nsalt.len(), SALT_SIZE); }
+    if nnonce.len() != NONCE_SIZE { panic!("Wrong nonce len {} should be {}", nnonce.len(), NONCE_SIZE) }
+    
+    let ndata = env.convert_byte_array(data).unwrap();
 
-  let pwd_str: String = env.get_string(password).expect("Couldn't get java string!").into();
-  let pwd: &[u8] = pwd_str.as_bytes();
+    let pwd_str: String = env.get_string(password).expect("Couldn't get java string!").into();
+    let pwd: &[u8] = pwd_str.as_bytes();
 
-  let result_size = ndata.len() + TAG_SIZE + NONCE_SIZE + SALT_SIZE;
+    let result_size = ndata.len() + TAG_SIZE + NONCE_SIZE + SALT_SIZE;
 
-  let mut output = Vec::with_capacity(result_size);
+    let mut output = Vec::with_capacity(result_size);
 
-  let rsz = encrypt_with_password(
-    pwd.as_ptr(), pwd.len(), nsalt.as_ptr(), nnonce.as_ptr(), ndata.as_ptr(), ndata.len(),
-    output.as_mut_ptr()
-  ) as usize;
+    let rsz = encrypt_with_password(
+      pwd.as_ptr(), pwd.len(), nsalt.as_ptr(), nnonce.as_ptr(), ndata.as_ptr(), ndata.len(),
+      output.as_mut_ptr()
+    ) as usize;
 
-  if rsz != result_size { panic!("Size mismatch {} should be {}", rsz, result_size) }
+    if rsz != result_size { panic!("Size mismatch {} should be {}", rsz, result_size) }
 
-  env.byte_array_from_slice(&output).unwrap()
+    env.byte_array_from_slice(&output).unwrap()
+  })
 }
 
 #[allow(non_snake_case)]
@@ -154,25 +177,27 @@ pub extern fn Java_io_crossroad_rncardano_Native_passwordProtectDecryptWithPassw
 ) -> jbyteArray {
   use self::password_encryption_parameter::*;
 
-  let ndata = env.convert_byte_array(data).unwrap();
+  handle_exception(env, |env| {
+    let ndata = env.convert_byte_array(data).unwrap();
 
-  if ndata.len() <= TAG_SIZE + NONCE_SIZE + SALT_SIZE { 
-    panic!("Wrong data len {} should be at least {}", ndata.len(), TAG_SIZE + NONCE_SIZE + SALT_SIZE + 1);
-  }
+    if ndata.len() <= TAG_SIZE + NONCE_SIZE + SALT_SIZE { 
+      panic!("Wrong data len {} should be at least {}", ndata.len(), TAG_SIZE + NONCE_SIZE + SALT_SIZE + 1);
+    }
 
-  let pwd_str: String = env.get_string(password).expect("Couldn't get java string!").into();
-  let pwd: &[u8] = pwd_str.as_bytes();
+    let pwd_str: String = env.get_string(password).expect("Couldn't get java string!").into();
+    let pwd: &[u8] = pwd_str.as_bytes();
 
-  let result_size = ndata.len() - TAG_SIZE - NONCE_SIZE - SALT_SIZE;
+    let result_size = ndata.len() - TAG_SIZE - NONCE_SIZE - SALT_SIZE;
 
-  let mut output = Vec::with_capacity(result_size);
+    let mut output = Vec::with_capacity(result_size);
 
-  let rsz = decrypt_with_password(
-    pwd.as_ptr(), pwd.len(), ndata.as_ptr(), ndata.len(),
-    output.as_mut_ptr()
-  ) as usize;
+    let rsz = decrypt_with_password(
+      pwd.as_ptr(), pwd.len(), ndata.as_ptr(), ndata.len(),
+      output.as_mut_ptr()
+    ) as usize;
 
-  if rsz != result_size { panic!("Size mismatch {} should be {}", rsz, result_size) }
+    if rsz != result_size { panic!("Size mismatch {} should be {}", rsz, result_size) }
 
-  env.byte_array_from_slice(&output).unwrap()
+    env.byte_array_from_slice(&output).unwrap()
+  })
 }
