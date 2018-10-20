55f469b036f4227631d0db3c4ffc8fa4f35abca5
Yehor Popovych <ypopovych@crossroadlabs.xyz>
2018-10-20 18:22:59 +0300

    Moved to npm published react java modules

--- android/src/main/kotlin/io/crossroad/rncardano/CardanoPackage.kt
+++ android/src/main/kotlin/io/crossroad/rncardano/CardanoPackage.kt
@@ -4,7 +4,6 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import com.facebook.react.bridge.JavaScriptModule
 
 class CardanoPackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
@@ -14,11 +13,6 @@ class CardanoPackage : ReactPackage {
                 PasswordProtect(reactContext),
                 RandomAddressChecker(reactContext)
         )
-    }
-
-    // Deprecated from RN 0.47
-    override fun createJSModules(): List<Class<out JavaScriptModule>> {
-        return emptyList()
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
