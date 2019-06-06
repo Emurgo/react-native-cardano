959daf2ed8ee08749c07d6adc242b87fd83ecc3b
Nicolas <nicolas@emurgo.io>
2019-06-06 17:01:34 -0400

    Fix armv7 compilation in Release mode

--- ios/rust/build.sh
+++ ios/rust/build.sh
@@ -16,9 +16,7 @@ fi
 
 cd "${SRCROOT}"/../rust
 
-env -i HOME="$HOME" LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" \
-    PATH="$PATH" USER="$USER" \
-    cargo lipo --release
+cargo lipo --xcode-integ
 
 cp -f "${SRCROOT}"/../rust/target/universal/release/*.a "${SRCROOT}"/rust/
 cp -f "${SRCROOT}"/../rust/include/*.h "${SRCROOT}"/rust/
